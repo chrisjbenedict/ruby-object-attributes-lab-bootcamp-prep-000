ecdaf6bb17e09912aced104ec8cf420c8009c5ad
Christopher Benedict <chrisjbenedict@gmail.com>
2018-05-08 16:05:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
   def initialize(name)
-    @name = name
+    @name= name
   end
   def name
     @name
   end
   def intialize(breed)
-    @breed = breed
+    @breed= breed
   end
   def breed
     @breed
